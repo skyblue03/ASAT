5e0457bec8170912f3e03714f544657e5de9986d
skyblue03 <ehsan.hasani@outlook.com.au>
2022-07-06 14:00:00 +0000

    Update main_window.py

--- views/main_window.py
+++ views/main_window.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QTextEdit, QFileDialog
-from models.sentiment_analyzer import analyze_sentiment
+from models.sentiment_analyzer import analyse_sentiment
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,20 +15,28 @@ class MainWindow(QMainWindow):
         self.load_button.clicked.connect(self.load_file)
         self.layout.addWidget(self.load_button)
 
-        self.analyze_button = QPushButton('Analyze Sentiment', self)
-        self.analyze_button.clicked.connect(self.on_analyze_clicked)
-        self.layout.addWidget(self.analyze_button)
+        self.load_multiple_button = QPushButton('Load Multiple Files', self)
+        self.load_multiple_button.clicked.connect(self.load_multiple_files)
+        self.layout.addWidget(self.load_multiple_button)
 
-        self.result_label = QLabel('Sentiment Result: ', self)
+        self.analyse_button = QPushButton('Analyse Sentiment', self)
+        self.analyse_button.clicked.connect(self.on_analyse_clicked)
+        self.layout.addWidget(self.analyse_button)
+
+        self.save_button = QPushButton('Save Results', self)
+        self.save_button.clicked.connect(self.save_results)
+        self.layout.addWidget(self.save_button)
+
+        self.result_label = QLabel('Sentiment Result:', self)
         self.layout.addWidget(self.result_label)
 
         container = QWidget()
         container.setLayout(self.layout)
         self.setCentralWidget(container)
 
-    def on_analyze_clicked(self):
+    def on_analyse_clicked(self):
         text = self.text_edit.toPlainText()
-        sentiment = analyze_sentiment(text)
+        sentiment = analyse_sentiment(text)
         self.result_label.setText(f"Sentiment Result: {sentiment}")
 
     def load_file(self):
@@ -37,3 +45,22 @@ class MainWindow(QMainWindow):
             with open(fname, 'r') as file:
                 data = file.read()
                 self.text_edit.setText(data)
+
+    def load_multiple_files(self):
+        files, _ = QFileDialog.getOpenFileNames(self, 'Open files', '/home', "Text files (*.txt);;CSV files (*.csv)")
+        if files:
+            results = []
+            for fname in files:
+                with open(fname, 'r') as file:
+                    data = file.read()
+                    sentiment = analyse_sentiment(data)
+                    results.append((fname, sentiment))
+            result_text = "\n".join(f"{fname}: {sentiment}" for fname, sentiment in results)
+            self.text_edit.setText(result_text)
+
+    def save_results(self):
+        fname, _ = QFileDialog.getSaveFileName(self, 'Save file', '/home', "Text files (*.txt)")
+        if fname:
+            with open(fname, 'w') as file:
+                data = self.text_edit.toPlainText()
+                file.write(data)

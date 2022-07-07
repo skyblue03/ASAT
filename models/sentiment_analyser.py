6fca245dbaf40cc63f7253a6fc01e7bf2a8f9b2d
skyblue03 <ehsan.hasani@outlook.com.au>
2022-07-07 12:30:00 +0000

    Switched to Transformers NPL

--- models/sentiment_analyser.py
+++ models/sentiment_analyser.py
@@ -1,21 +1,7 @@
-import spacy
+from transformers import pipeline
 
-# Load the pre-trained spaCy model
-nlp = spacy.load('en_core_web_sm')
+nlp = pipeline("sentiment-analysis")
 
-def analyze_sentiment(text):
-    """
-    Analyze sentiment of the provided text using spaCy.
-    
-    Args:
-        text (str): Text to analyze.
-
-    Returns:
-        str: Sentiment of the text (Positive, Negative, Neutral).
-    """
-    doc = nlp(text)
-    # This is a placeholder for the sentiment analysis logic.
-    # You might want to integrate a more advanced sentiment analysis here.
-    sentiment = "Neutral"  # Default to neutral for demonstration.
-    # Analyze the doc and determine sentiment.
-    return sentiment
+def analyse_sentiment(text):
+    result = nlp(text)[0]
+    return result['label']

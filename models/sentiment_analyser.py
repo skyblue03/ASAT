baea5c4d3d90ddf06068031a32528a9fa09f29ba
skyblue03 <ehsan.hasani@outlook.com.au>
2022-07-09 13:00:00 +0000

    Emotion detection

--- models/sentiment_analyser.py
+++ models/sentiment_analyser.py
@@ -1,7 +1,15 @@
 from transformers import pipeline
 
+# Load pre-trained sentiment analysis model
 nlp = pipeline("sentiment-analysis")
+
+# Load pre-trained emotion detection model
+emotion_model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
 
 def analyse_sentiment(text):
     result = nlp(text)[0]
     return result['label']
+
+def detect_emotion(text):
+    emotions = emotion_model(text)
+    return emotions
--- views/main_window.py
+++ views/main_window.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QTextEdit, QFileDialog, QLabel, QVBoxLayout, QWidget
-from models.sentiment_analyser import analyse_sentiment
+from models.sentiment_analyser import analyse_sentiment, detect_emotion
 from .mpl_widget import MplWidget
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -24,12 +25,19 @@ class MainWindow(QMainWindow):
         self.analyse_button.clicked.connect(self.on_analyse_clicked)
         self.layout.addWidget(self.analyse_button)
 
+        self.emotion_button = QPushButton('Detect Emotions', self)
+        self.emotion_button.clicked.connect(self.on_detect_emotion_clicked)
+        self.layout.addWidget(self.emotion_button)
+
         self.save_button = QPushButton('Save Results', self)
         self.save_button.clicked.connect(self.save_results)
         self.layout.addWidget(self.save_button)
 
         self.result_label = QLabel('Sentiment Result:', self)
         self.layout.addWidget(self.result_label)
+
+        self.emotion_label = QLabel('Emotion Detection Result:', self)
+        self.layout.addWidget(self.emotion_label)
 
         container = QWidget()
         container.setLayout(self.layout)
@@ -39,6 +47,12 @@ class MainWindow(QMainWindow):
         text = self.text_edit.toPlainText()
         sentiment = analyse_sentiment(text)
         self.result_label.setText(f"Sentiment Result: {sentiment}")
+
+    def on_detect_emotion_clicked(self):
+        text = self.text_edit.toPlainText()
+        emotions = detect_emotion(text)
+        emotion_result = "\n".join([f"{emotion['label']}: {emotion['score']:.2f}" for emotion in emotions[0]])
+        self.emotion_label.setText(f"Emotion Detection Result:\n{emotion_result}")
 
     def load_file(self):
         fname, _ = QFileDialog.getOpenFileName(self, 'Open file', '/home', "Text files (*.txt);;CSV files (*.csv)")

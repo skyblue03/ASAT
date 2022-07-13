dd92895f7b0f22bde06d8f8fc9905265309bf53e
skyblue03 <ehsan.hasani@outlook.com.au>
2022-07-13 11:50:00 +0000

    Report Generation and model tokens limit

--- models/sentiment_analyser.py
+++ models/sentiment_analyser.py
@@ -1,23 +1,88 @@
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer
 
-# Load pre-trained sentiment analysis model
-nlp = pipeline("sentiment-analysis")
+# Load pre-trained models
+nlp = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+emotion_model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
+ner_model = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english", aggregation_strategy="simple")
 
-# Load pre-trained emotion detection model
-emotion_model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
+# Load tokenizers
+sentiment_tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+emotion_tokenizer = AutoTokenizer.from_pretrained("j-hartmann/emotion-english-distilroberta-base")
+ner_tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-large-cased-finetuned-conll03-english")
 
-# NER model
-ner_model = pipeline("ner", grouped_entities=True)
+def chunk_text(text, tokenizer, chunk_size=512):
+    tokens = tokenizer(text, return_tensors='pt', truncation=True, padding=True, max_length=chunk_size)
+    for i in range(0, len(tokens['input_ids'][0]), chunk_size):
+        chunk = tokens['input_ids'][0][i:i + chunk_size]
+        yield tokenizer.decode(chunk, skip_special_tokens=True)
 
 def analyse_sentiment(text):
-    result = nlp(text)[0]
-    return result['label']
+    sentiments = []
+    try:
+        for chunk in chunk_text(text, sentiment_tokenizer):
+            result = nlp(chunk)[0]
+            sentiments.append(result['label'])
+        
+        # Aggregate results
+        sentiment_counts = {sentiment: sentiments.count(sentiment) for sentiment in set(sentiments)}
+        most_common_sentiment = max(sentiment_counts, key=sentiment_counts.get)
+        return most_common_sentiment
+    except Exception as e:
+        return f"Error in sentiment analysis: {str(e)}"
 
 def detect_emotion(text, threshold=0.05):
-    emotions = emotion_model(text)
-    significant_emotions = [emotion for emotion in emotions[0] if emotion['score'] > threshold]
-    return significant_emotions
+    try:
+        emotions = []
+        for chunk in chunk_text(text, emotion_tokenizer):
+            chunk_emotions = emotion_model(chunk)[0]
+            emotions.extend(chunk_emotions)
+        significant_emotions = [emotion for emotion in emotions if emotion['score'] > threshold]
+        return significant_emotions
+    except Exception as e:
+        return f"Error in emotion detection: {str(e)}"
 
 def recognize_entities(text):
-    entities = ner_model(text)
-    return entities
+    try:
+        entities = []
+        for chunk in chunk_text(text, ner_tokenizer):
+            chunk_entities = ner_model(chunk)
+            entities.extend(chunk_entities)
+        return entities
+    except Exception as e:
+        return f"Error in entity recognition: {str(e)}"
+
+def generate_pdf_report(data, filename="report.pdf"):
+    from reportlab.lib.pagesizes import letter
+    from reportlab.pdfgen import canvas
+
+    try:
+        c = canvas.Canvas(filename, pagesize=letter)
+        width, height = letter
+        c.drawString(100, height - 40, "Sentiment Analysis Report")
+        c.drawString(100, height - 60, f"Sentiment: {data['sentiment']}")
+        c.drawString(100, height - 80, "Emotion Scores:")
+        y = height - 100
+        for emotion, score in data['emotions'].items():
+            c.drawString(100, y, f"{emotion}: {score:.2f}")
+            y -= 20
+        c.drawString(100, y, "Entities:")
+        y -= 20
+        for entity in data['entities']:
+            c.drawString(100, y, f"{entity['entity_group']}: {entity['word']} (score: {entity['score']:.2f})")
+            y -= 20
+        c.save()
+    except Exception as e:
+        print(f"Error in generating PDF report: {str(e)}")
+
+def generate_excel_report(data, filename="report.xlsx"):
+    import pandas as pd
+
+    try:
+        writer = pd.ExcelWriter(filename, engine='openpyxl')
+        df = pd.DataFrame([data['emotions']])
+        df.to_excel(writer, index=False, sheet_name='Emotions')
+        entities_df = pd.DataFrame(data['entities'])
+        entities_df.to_excel(writer, index=False, sheet_name='Entities')
+        writer.save()
+    except Exception as e:
+        print(f"Error in generating Excel report: {str(e)}")
--- views/main_window.py
+++ views/main_window.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QTextEdit, QFileDialog, QLabel, QVBoxLayout, QWidget
-from models.sentiment_analyser import analyse_sentiment, detect_emotion, recognize_entities
+from models.sentiment_analyser import analyse_sentiment, detect_emotion, recognize_entities, generate_pdf_report, generate_excel_report
 from .mpl_widget import MplWidget
 
 class MainWindow(QMainWindow):
@@ -32,9 +32,13 @@ class MainWindow(QMainWindow):
         self.entity_button.clicked.connect(self.on_recognize_entities_clicked)
         self.layout.addWidget(self.entity_button)
 
-        self.save_button = QPushButton('Save Results', self)
-        self.save_button.clicked.connect(self.save_results)
-        self.layout.addWidget(self.save_button)
+        self.save_pdf_button = QPushButton('Save PDF Report', self)
+        self.save_pdf_button.clicked.connect(self.on_save_pdf_report_clicked)
+        self.layout.addWidget(self.save_pdf_button)
+
+        self.save_excel_button = QPushButton('Save Excel Report', self)
+        self.save_excel_button.clicked.connect(self.on_save_excel_report_clicked)
+        self.layout.addWidget(self.save_excel_button)
 
         self.result_label = QLabel('Sentiment Result:', self)
         self.layout.addWidget(self.result_label)
@@ -52,33 +56,67 @@ class MainWindow(QMainWindow):
         container.setLayout(self.layout)
         self.setCentralWidget(container)
 
+        # Initialize current data attributes
+        self.current_sentiment = None
+        self.current_emotions = None
+        self.current_entities = None
+
     def on_analyse_clicked(self):
         text = self.text_edit.toPlainText()
         sentiment = analyse_sentiment(text)
         self.result_label.setText(f"Sentiment Result: {sentiment}")
         self.update_plot({'Positive': 10, 'Neutral': 5, 'Negative': 3})  # Example data
+        self.current_sentiment = sentiment
 
     def on_detect_emotion_clicked(self):
         text = self.text_edit.toPlainText()
         emotions = detect_emotion(text)
-        emotion_result = "\n".join([f"{emotion['label']}: {emotion['score']:.2f}" for emotion in emotions])
-        self.emotion_label.setText(f"Emotion Detection Result:\n{emotion_result}")
+        if isinstance(emotions, str):
+            self.emotion_label.setText(emotions)  # Display error message
+        else:
+            emotion_result = "\n".join([f"{emotion['label']}: {emotion['score']:.2f}" for emotion in emotions])
+            self.emotion_label.setText(f"Emotion Detection Result:\n{emotion_result}")
+            self.current_emotions = {emotion['label']: emotion['score'] for emotion in emotions}
 
     def on_recognize_entities_clicked(self):
         text = self.text_edit.toPlainText()
         entities = recognize_entities(text)
-        entity_result = "\n".join([f"{entity['entity_group']}: {entity['word']} (score: {entity['score']:.2f})" for entity in entities])
-        self.entity_label.setText(f"Entity Recognition Result:\n{entity_result}")
+        if isinstance(entities, str):
+            self.entity_label.setText(entities)  # Display error message
+        else:
+            entity_result = "\n".join([f"{entity['entity_group']}: {entity['word']} (score: {entity['score']:.2f})" for entity in entities])
+            self.entity_label.setText(f"Entity Recognition Result:\n{entity_result}")
+            self.current_entities = [{'entity_group': entity['entity_group'], 'word': entity['word'], 'score': entity['score']} for entity in entities]
+
+    def on_save_pdf_report_clicked(self):
+        data = {
+            'sentiment': self.current_sentiment,
+            'emotions': self.current_emotions,
+            'entities': self.current_entities
+        }
+        fname, _ = QFileDialog.getSaveFileName(self, 'Save PDF Report', '', "PDF files (*.pdf)")
+        if fname:
+            generate_pdf_report(data, fname)
+
+    def on_save_excel_report_clicked(self):
+        data = {
+            'sentiment': self.current_sentiment,
+            'emotions': self.current_emotions,
+            'entities': self.current_entities
+        }
+        fname, _ = QFileDialog.getSaveFileName(self, 'Save Excel Report', '', "Excel files (*.xlsx)")
+        if fname:
+            generate_excel_report(data, fname)
 
     def load_file(self):
-        fname, _ = QFileDialog.getOpenFileName(self, 'Open file', '/home', "Text files (*.txt);;CSV files (*.csv)")
+        fname, _ = QFileDialog.getOpenFileName(self, 'Open file', '', "Text files (*.txt);;CSV files (*.csv)")
         if fname:
             with open(fname, 'r') as file:
                 data = file.read()
                 self.text_edit.setText(data)
 
     def load_multiple_files(self):
-        files, _ = QFileDialog.getOpenFileNames(self, 'Open files', '/home', "Text files (*.txt);;CSV files (*.csv)")
+        files, _ = QFileDialog.getOpenFileNames(self, 'Open files', '', "Text files (*.txt);;CSV files (*.csv)")
         if files:
             results = []
             for fname in files:
@@ -90,7 +128,7 @@ class MainWindow(QMainWindow):
             self.text_edit.setText(result_text)
 
     def save_results(self):
-        fname, _ = QFileDialog.getSaveFileName(self, 'Save file', '/home', "Text files (*.txt)")
+        fname, _ = QFileDialog.getSaveFileName(self, 'Save file', '', "Text files (*.txt)")
         if fname:
             with open(fname, 'w') as file:
                 data = self.text_edit.toPlainText()
